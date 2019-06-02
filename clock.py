d9784e55b1d4e92568753a80ce3189f09e3b66f5
vincent50788 <vincent50788@yahoo.com.tw>
2019-06-02 14:27:11 +0800

    clockv3

--- clock.py
+++ clock.py
@@ -11,10 +11,12 @@ from superCrawler import *
 
 timez = pytz.timezone('Asia/Taipei')
 
-scheduler = BlockingScheduler(timezone=timez)
+scheduler = BlockingScheduler()
 scheduler.add_job(weather_crawler, trigger='interval', minutes=20)
+
 scheduler.add_job(oil_crawler, trigger='cron', hour='0-1', minute='0-59')
-scheduler.add_job(alert_crawler, trigger='interval', minute=5)
+
+scheduler.add_job(alert_crawler, 'interval', minutes=5)
 scheduler.start()
 
 
